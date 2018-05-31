1b05b60a83e1ed5607e3b3a66310f9df3b621722
godning3 <736397792@qq.com>
2018-05-31 23:44:13 +0800

    add nav

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -7,6 +7,14 @@
           <img :src="logo.src" :alt="logo.title">
         </a>
       </div>
+
+      <div id="top-navbar-collapse" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li v-for="(item, index) in navList" :class="{ active: index === activeNavIndex }">
+            <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -19,15 +27,23 @@ export default {
       logo: {
         src: `${this.uploadsUrl}sites/ByvFbNlQYVwhvTyBgLdqitchoacDNznN.jpg`,
         title: 'VuejsCaff'
-      }
+      },
+      navList: ['社区', '头条', '问答', '教程'],
+      activeNavIndex: 0
     }
   },
   beforeCreate() {
     this.uploadsUrl = 'https://vuejscaffcdn.phphub.org/uploads/'
+  },
+  methods: {
+    changeNavIndex(index) {
+      this.activeNavIndex = index
+    }
   }
 }
 </script>
 
 <style scoped>
 .title { display: none;}
+.navbar-default .navbar-nav > .active > a { background: rgba(0,0,0,.03);}
 </style>
